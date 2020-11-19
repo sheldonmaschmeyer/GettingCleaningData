1ba02caec85459dbc9ab989b5d42728499dc3011
Sheldon <s2maschmeyer@gmail.com>
2020-11-19 00:19:53 -0500

    garbage collection

--- run_analysis.R
+++ run_analysis.R
@@ -8,8 +8,6 @@
 #5. From the data set in step 4, creates a second, independent tidy data set with the average of 
 #each variable for each activity and each subject.
 
-library(filesstrings) #For moving files.
-
 if(!file.exists("dataset.zip")) {
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl, destfile = "dataset.zip")
